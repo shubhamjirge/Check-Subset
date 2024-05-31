b449653caf435f8eeaaf409c75868b953c150e3d
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-05-31 12:17:46 +0530

    Add files via upload

---
+++ CheckSubset.py
@@ -0,0 +1,13 @@
+"""
+Title     : Check Subset
+Subdomain : Sets
+Domain    : Python
+"""
+
+number_of_testcases = int(input())
+for _ in range(number_of_testcases):
+    number_of_elements_first_set = int(input())
+    first_set = set(map(int, input().split()))
+    number_of_elements_second_set = int(input())
+    second_set = set(map(int, input().split()))
+    print(first_set.issubset(second_set))
